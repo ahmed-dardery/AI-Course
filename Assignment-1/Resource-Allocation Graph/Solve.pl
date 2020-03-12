5acb3d19ceb031c618a3e1cc52503cf1d76f43b0
Ahmed Dardery <Megadardery@yahoo.com>
2020-03-12 15:37:58 +0200

    removing given data

--- Assignment-1/Resource-Allocation Graph/Solve.pl
+++ Assignment-1/Resource-Allocation Graph/Solve.pl
@@ -1,15 +1,3 @@
-processes([p1,p2,p3,p4]).
-
-available_resources([[r1, 0], [r2, 0]]).
-
-allocated(p1, [r2]).
-allocated(p2, [r1]).
-allocated(p3, [r1]).
-allocated(p4, [r2]).
-
-requested(p1, [r1]).
-requested(p3, [r2]).
-
 % Counts number of processes in system:
 processCnt(N):-processes(List), processCnt(List, N, 0).
 processCnt([],N,N).
